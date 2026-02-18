a1134511538d0bbff75cd6d561818e9754e03df5
Dominik Krajcer <dom.kraj@gmail.com>
2026-02-18 20:42:26 +0100

    feat(lint): add avoid_border_all rule with quick fix

--- lib/many_lints.dart
+++ lib/many_lints.dart
@@ -24,6 +24,7 @@ import 'package:analysis_server_plugin/registry.dart';
 // Rules
 import 'package:many_lints/src/rules/always_remove_listener.dart';
 import 'package:many_lints/src/rules/avoid_cascade_after_if_null.dart';
+import 'package:many_lints/src/rules/avoid_border_all.dart';
 import 'package:many_lints/src/rules/avoid_single_field_destructuring.dart';
 import 'package:many_lints/src/rules/avoid_flexible_outside_flex.dart';
 import 'package:many_lints/src/rules/avoid_constant_conditions.dart';
@@ -90,6 +91,7 @@ import 'package:many_lints/src/rules/use_notifier_suffix.dart';
 import 'package:many_lints/src/fixes/always_remove_listener_fix.dart';
 import 'package:many_lints/src/fixes/dispose_fields_fix.dart';
 import 'package:many_lints/src/fixes/avoid_cascade_after_if_null_fix.dart';
+import 'package:many_lints/src/fixes/avoid_border_all_fix.dart';
 import 'package:many_lints/src/fixes/avoid_single_field_destructuring_fix.dart';
 import 'package:many_lints/src/fixes/avoid_commented_out_code_fix.dart';
 import 'package:many_lints/src/fixes/avoid_incomplete_copy_with_fix.dart';
@@ -212,6 +214,7 @@ class ManyLintsPlugin extends Plugin {
     registry.registerWarningRule(UseExistingDestructuring());
     registry.registerWarningRule(UseExistingVariable());
     registry.registerWarningRule(AvoidSingleFieldDestructuring());
+    registry.registerWarningRule(AvoidBorderAll());
 
     // Register fixes for rules
     registry.registerFixForRule(
@@ -384,6 +387,7 @@ class ManyLintsPlugin extends Plugin {
       AvoidSingleFieldDestructuring.code,
       AvoidSingleFieldDestructuringFix.new,
     );
+    registry.registerFixForRule(AvoidBorderAll.code, AvoidBorderAllFix.new);
 
     // Register assists
     registry.registerAssist(ConvertIterableMapToCollectionFor.new);
